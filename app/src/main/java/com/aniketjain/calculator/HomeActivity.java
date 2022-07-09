04a3068c083c86e567f352da8657fe0620dca7e2
aniketjain8441 <aniketjain8441@gmail.com>
2022-07-09 23:11:14 +0530

    new changes

--- app/src/main/java/com/aniketjain/calculator/HomeActivity.java
+++ app/src/main/java/com/aniketjain/calculator/HomeActivity.java
@@ -29,31 +29,58 @@ public class HomeActivity extends AppCompatActivity {
 
     }
 
+    @SuppressLint("SetTextI18n")
     private void onClickListeners() {
-        binding.btn0.setOnClickListener(view -> setInputData("0"));
-        binding.btn1.setOnClickListener(view -> setInputData("1"));
-        binding.btn2.setOnClickListener(view -> setInputData("2"));
-        binding.btn3.setOnClickListener(view -> setInputData("3"));
-        binding.btn4.setOnClickListener(view -> setInputData("4"));
-        binding.btn5.setOnClickListener(view -> setInputData("5"));
-        binding.btn6.setOnClickListener(view -> setInputData("6"));
-        binding.btn7.setOnClickListener(view -> setInputData("7"));
-        binding.btn8.setOnClickListener(view -> setInputData("8"));
-        binding.btn9.setOnClickListener(view -> setInputData("9"));
 
+        // ROW 1
         binding.btnAc.setOnClickListener(view -> {
             binding.inputText.setText("");
             binding.outputText.setText("");
         });
         binding.btnPlusMinus.setOnClickListener(view -> {
+            data = binding.inputText.getText().toString();
+            if (data.charAt(0) != '-') {
+                binding.inputText.setText("-" + data);
+            } else {
+                StringBuilder new_data = new StringBuilder();
+                for (int i = 1; i < data.length(); i++)
+                    new_data = new_data.append(data.charAt(i));
+                binding.inputText.setText(new_data.toString());
+            }
         });
         binding.btnPerc.setOnClickListener(view -> setInputData("%"));
         binding.btnDiv.setOnClickListener(view -> setInputData("÷"));
+
+        // ROW 2
+        binding.btn7.setOnClickListener(view -> setInputData("7"));
+        binding.btn8.setOnClickListener(view -> setInputData("8"));
+        binding.btn9.setOnClickListener(view -> setInputData("9"));
         binding.btnMul.setOnClickListener(view -> setInputData("x"));
+
+        // ROW 3
+        binding.btn4.setOnClickListener(view -> setInputData("4"));
+        binding.btn5.setOnClickListener(view -> setInputData("5"));
+        binding.btn6.setOnClickListener(view -> setInputData("6"));
         binding.btnMinus.setOnClickListener(view -> setInputData("-"));
+
+        // ROW 4
+        binding.btn1.setOnClickListener(view -> setInputData("1"));
+        binding.btn2.setOnClickListener(view -> setInputData("2"));
+        binding.btn3.setOnClickListener(view -> setInputData("3"));
         binding.btnPlus.setOnClickListener(view -> setInputData("+"));
-        binding.btnEqual.setOnClickListener(view -> doCalculation());
+
+        // ROW 5
+        binding.btnDel.setOnClickListener(view -> {
+            data = binding.inputText.getText().toString();
+            StringBuilder new_data = new StringBuilder();
+            for (int i = 0; i < data.length() - 1; i++)
+                new_data = new_data.append(data.charAt(i));
+            binding.inputText.setText(new_data.toString());
+        });
+        binding.btn0.setOnClickListener(view -> setInputData("0"));
         binding.btnDot.setOnClickListener(view -> setInputData("."));
+        binding.btnEqual.setOnClickListener(view -> doCalculation());
+
     }
 
     @SuppressLint("SetTextI18n")
